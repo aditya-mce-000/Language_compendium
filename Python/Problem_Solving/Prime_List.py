1c0c7738ff66a1482576b5ae41641fa4e38e55d3
cseaditya-000 <aditya3546raj@gmail.com>
2026-09-07 23:07:25 +0530

    -

--- Python/Problem_Solving/Prime_List.py
+++ Python/Problem_Solving/Prime_List.py
@@ -1,8 +1,5 @@
-lst = [i for i in range(2,2000)]
-print(lst)
-# for j in lst:
-#     print(j)
-lst1 = lst.remove([i for i in range(3,2000,3)])
-print(lst1)
-# for i in lst1:
-    # print(i)
+lst = [i for i in range(2,1000)]
+unmarked = 2
+
+for i in lst:
+    
